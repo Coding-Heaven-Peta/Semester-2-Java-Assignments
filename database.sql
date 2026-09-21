c122e184fbb8e31dccd79c2daf0e0542b134102d
Coding-Heaven-Peta <davidpeta958@gmail.com>
2026-09-21 19:16:07 +0200

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,2 @@
+INSERT INTO medicines (name, company, medicine_type, price, quantity_in_stock, reorder_level, expiry_date, supplier_id)
+VALUES ('Aspirin', 'Bayer', 'Tablet', 12.99, 5, 10, '2026-10-01', 1);
